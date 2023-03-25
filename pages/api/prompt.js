c8705a1adbe48b5f8dacb6463e180e45ae8e40c1
jtwirly <jenn.turliuk@gmail.com>
2023-03-24 23:20:14 -0400

    Saving to supabase working

--- pages/api/prompt.js
+++ pages/api/prompt.js
@@ -1,24 +1,25 @@
 import { nanoid } from 'nanoid';
 import { supabase } from '../../lib/supabase';
+import { v4 as uuidv4 } from 'uuid';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const prompt = req.body.prompt;
+    const id = uuidv4(); // Generate a unique ID for the new record
+    const { title, subtitle, inputplaceholder, buttonname, prompt, disclaimer } = req.body;
     const response = await fetch(
-        'https://api.openai.com/v1/engines/text-davinci-003/completions',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          },
-          body: JSON.stringify({
-            prompt,
-            max_tokens: 64,
-          }),
-        }
-      )
-      
+      'https://api.openai.com/v1/engines/text-davinci-003/completions',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          prompt,
+          max_tokens: 64,
+        }),
+      }
+    )
       .then((res) => {
         console.log('Response from OpenAI API:', res);
         return res.json();
@@ -33,7 +34,7 @@ export default async function handler(req, res) {
       });
 
     const url = `/${nanoid()}`;
-    const { data, error } = await supabase.from('prompts').insert([{ prompt, response, url }]);
+    const { data, error } = await supabase.from('webapps').insert([{ id, title, subtitle, inputplaceholder, buttonname, prompt, disclaimer, url }]);
     if (error) {
       console.log('Error saving prompt:', error.message);
       res.status(500).json({ error: 'Internal Server Error' });
